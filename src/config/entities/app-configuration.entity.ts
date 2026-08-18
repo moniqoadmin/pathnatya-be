dc4f41647cdac3eef89b9607381f3ae41bfd915d
moniqoadmin <moniqo.admin@gmail.com>
2026-08-18 14:50:28 +0530

    add app configuration entity

---
+++ src/config/entities/app-configuration.entity.ts
@@ -0,0 +1,18 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+export interface VideoConfig {
+  DEFAULT_HLS_SOURCE: string;
+  ALLOWED_HOSTS: string[];
+}
+
+@Entity('app_configurations')
+export class AppConfiguration {
+  @PrimaryGeneratedColumn({ type: 'int' })
+  id: number;
+
+  @Column({ name: 'video_config', type: 'jsonb' })
+  videoConfig: VideoConfig;
+
+  @Column({ name: 'video_files', type: 'jsonb', default: () => "'[]'::jsonb" })
+  videoFiles: unknown[];
+}
