306cf8808ca0db2f76624f7cf5bd5e738987df49
moniqoadmin <moniqo.admin@gmail.com>
2026-08-18 14:53:38 +0530

    register configuration entities for db sync

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { timingSafeEqual } from 'crypto';
 import { buildCacheConfig } from './config/cache.config';
 import { buildDatabaseConfig } from './config/database.config';
+import { AppConfiguration } from './config/entities/app-configuration.entity';
+import { ServerApiUrl } from './config/entities/server-api-url.entity';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HealthModule } from './health/health.module';
@@ -70,6 +72,7 @@ function loadTestKeyMatches(
       inject: [ConfigService],
       useFactory: (config: ConfigService) => buildDatabaseConfig(config),
     }),
+    TypeOrmModule.forFeature([AppConfiguration, ServerApiUrl]),
     CacheModule.registerAsync({
       isGlobal: true,
       inject: [ConfigService],
