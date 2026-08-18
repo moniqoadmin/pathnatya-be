e23802bb1ed9f2d43be09065b18311d18c4e5171
moniqoadmin <moniqo.admin@gmail.com>
2026-08-18 15:00:26 +0530

    wire app configuration APIs

--- src/config/configuration-storage.module.ts
+++ src/config/configuration-storage.module.ts
@@ -1,13 +1,19 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AccountsModule } from '../accounts/accounts.module';
+import { AppConfigurationsController } from './app-configurations.controller';
+import { AppConfigurationsService } from './app-configurations.service';
 import { AppConfiguration } from './entities/app-configuration.entity';
 import { ServerApiUrl } from './entities/server-api-url.entity';
 import { ServerApiUrlsController } from './server-api-urls.controller';
 import { ServerApiUrlsService } from './server-api-urls.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([AppConfiguration, ServerApiUrl])],
-  controllers: [ServerApiUrlsController],
-  providers: [ServerApiUrlsService],
+  imports: [
+    TypeOrmModule.forFeature([AppConfiguration, ServerApiUrl]),
+    AccountsModule,
+  ],
+  controllers: [AppConfigurationsController, ServerApiUrlsController],
+  providers: [AppConfigurationsService, ServerApiUrlsService],
 })
 export class ConfigurationStorageModule {}
