9e3fd5e45ed3473a8b400fe0375a84133c3025a2
moniqoadmin <moniqo.admin@gmail.com>
2026-08-18 12:18:37 +0530

    Make check-phone team aware

--- src/accounts/dto/check-phone.dto.ts
+++ src/accounts/dto/check-phone.dto.ts
@@ -1,8 +1,17 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+import { IsInt, Max, Min } from 'class-validator';
 import { IsSupportedPhoneNumber } from '../validators/supported-phone-number.validator';
 
 export class CheckPhoneDto {
   @ApiProperty({ example: '9876543210' })
   @IsSupportedPhoneNumber()
   phoneNumber: string;
+
+  @ApiProperty({ example: 1, minimum: 1, maximum: 20 })
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  @Max(20)
+  teamNumber: number;
 }
